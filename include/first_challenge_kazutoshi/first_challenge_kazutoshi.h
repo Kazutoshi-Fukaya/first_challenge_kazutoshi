acfc35db43baf447b820a7fe453bf4bd2d2f78fd
shintani <kazutoshi4423@gmail.com>
2021-02-24 16:10:00 +0900

    roomba controller test01

---
+++ include/first_challenge_kazutoshi/first_challenge_kazutoshi.h
@@ -0,0 +1,27 @@
+#ifndef ROOMBA_CONTROLLER_H
+#define ROOMBA_CONTROLLER_H
+
+#include "ros/ros.h"
+#include "nav_msgs/Odometry.h"
+#include "roomba_500driver_meiji/RoombaCtrl.h"
+
+class RoombaController
+{
+    public:
+        MyTurtlesimController();
+        void process();
+
+    private:
+        void pose_callback(const nav_msgs::Odometry::ConstPtr &msg);
+        void go_straight();
+        void turn();
+
+        int hz_;
+
+        ros::NodeHandle nh;
+        ros::NodeHandle private_nh;
+        ros::Subscriber sub_pose;
+        ros::Publisher pub_cmd_vel;
+        geometry_msgs::Pose current_pose;
+};
+#endif
---
+++ src/roomba_controller.cpp
@@ -0,0 +1,58 @@
+#include "roomba_controller/roomba_controller.h"
+
+RoombaController::RoombaController():private_nh("~")
+{
+    private_nh.param'"hz",hz_,{10});
+
+    sub_pose = nh.subscribe("/roomba/odometry",10,&RoombaController::odometry_callback,this);
+
+    pub_cmd_vel = nh.advertise<roomba_500driver_meiji::RoombaCtrl>("/roomba/control",1);
+}
+
+void RoombaController::odometry_callback(const nav_msgs::Odometry::ConstPtr &msg)
+{
+    current_pose = *msg->pose.pose;
+}
+
+void RoombaController::go_straight()
+{
+    std::cout<<current_pose<<std::endl;
+    roomba_500driver_meiji::RoombaCtrl cmd_vel;
+    cmd_vel.ctrl.linear.x = 1.0;
+    cmd_vel.mode = 11;
+    pub_cmd_vel.publish(cmd_vel);
+}
+
+void RoombaController::turn()
+{
+    std::cout<<current_pose<<std::endl;
+    roomba_500driver_meiji/RoombaCtrl cmd_vel;
+    cmd_vel.ctrl.angular.z = 0.1;
+    cmd_vel.mode = 11;
+    pub_cmd_vel.publish(cmd_vel);
+}
+
+void RoombaController::process()
+{
+    ros::Rate loop_rate(hz_);
+    geometry::Pose past_pose = current_pose;
+    int straight_ = 0
+
+    while(ros::ok())
+    {
+        if ((current_pose.position.x-past_pose.position.x)*(current_pose.position.x-past_pose.position.x)+(current_pose.position.y-past_pose.position.y)*(current_pose.position.y-past_pose.pos    ition.y) < 1.0) straight = 1;
+
+        if (straight == 1) go_straight();
+
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+}
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv,"roomba_controller");
+    RoombaController roomba_controller;
+    roomba_controller.process();
+    return 0;
+}
